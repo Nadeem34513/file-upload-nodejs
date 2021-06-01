41a7e50a7b5315e92e210e36b3bced47cdbf7404
Nadeem <nadeembinnoushad12@gmail.com>
2021-06-02 00:20:16 +0530

    backendp

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const multer = require('multer')
+const path = require('path')
+
+const app = express()
+
+//Middlewares
+app.use(express.static('public'))
+app.set(express.urlencoded({extended: true}))
+
+// Storage setup
+const storage = multer.diskStorage({
+    destination: './public/uploads',
+    filename: (req, file, cb) => {
+        // returns new file with timestamp in the end
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+
+// init upload
+const upload = multer({
+    storage: storage,
+}).single('myImg')
+
+//Routes
+app.get('/', (req, res) => {
+    res.sendFile(__dirname, '/public/index.html')
+})
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(req.file)
+            res.send('SUCCESS')
+        }
+    })
+})
+
+//
+
+//PORT
+const PORT = 8000
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
